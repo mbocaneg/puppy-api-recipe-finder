1c1ab1fe0042144df076cdc0d04d172ef63a4d09
mbocaneg <mbocaneg@hawk.iit.edu>
2020-09-28 19:32:25 -0500

    update readme

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
-# puppy-api-recipe-finder
+# Demo
+https://mbocaneg.github.io/puppy-api-recipe-finder/
+
+# Recipe Finder
+A react based WebApp that queries and fetches recipes from the Recipe-Puppy Api.
+
+# Usage
+A user may search for recipes using any combination of name, ingredients, and
+dietary restrictions. Results are presented as a list of stylish recipe cards.
+
+# Building/Running
+Install project dependencies
+<br>
+```npm install```
+<br>
+Start the app by running:
+<br>
+```npm start```
---
+++ build/precache-manifest.aa4659a28991beebeca3d8804035412d.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a6920d8bf74571d3e98375b24f4948d1",
+    "url": "/puppy-api-recipe-finder/index.html"
+  },
+  {
+    "revision": "fb457e2cfa1bca876f6e",
+    "url": "/puppy-api-recipe-finder/static/css/main.ad9a2c84.chunk.css"
+  },
+  {
+    "revision": "955edd90f19efe7341f6",
+    "url": "/puppy-api-recipe-finder/static/js/2.28630ee3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/puppy-api-recipe-finder/static/js/2.28630ee3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fb457e2cfa1bca876f6e",
+    "url": "/puppy-api-recipe-finder/static/js/main.8bce4fab.chunk.js"
+  },
+  {
+    "revision": "9c3fcc6cae497c9e38d3",
+    "url": "/puppy-api-recipe-finder/static/js/runtime-main.1454b7fb.js"
+  },
+  {
+    "revision": "a18b37bac3312a030763dfd2d83177c2",
+    "url": "/puppy-api-recipe-finder/static/media/FushaBrush.a18b37ba.ttf"
+  },
+  {
+    "revision": "b2b65941ef26adcef861642f6e93eca3",
+    "url": "/puppy-api-recipe-finder/static/media/KitchenHome.b2b65941.otf"
+  },
+  {
+    "revision": "07d2bdb20c119313026e194f0b3e382b",
+    "url": "/puppy-api-recipe-finder/static/media/Petrona-Regular.07d2bdb2.ttf"
+  },
+  {
+    "revision": "ee65ac261ffd9f8c0072c135b36d3cfb",
+    "url": "/puppy-api-recipe-finder/static/media/YummyCupcakes.ee65ac26.ttf"
+  },
+  {
+    "revision": "ffb2a572f0cf7e93b2817a50fc7cb859",
+    "url": "/puppy-api-recipe-finder/static/media/chef.ffb2a572.svg"
+  },
+  {
+    "revision": "bc2294f981e4bab79922da8bac2f66b5",
+    "url": "/puppy-api-recipe-finder/static/media/dish1.bc2294f9.jpg"
+  },
+  {
+    "revision": "7fb95eef78c921b7c0521b5f89e8516c",
+    "url": "/puppy-api-recipe-finder/static/media/dish5.7fb95eef.jpg"
+  }
+]);
